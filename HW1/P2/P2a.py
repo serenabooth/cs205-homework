afea259c31c0c8d5e4d723a1300d911f0d4eba52
Serena Booth <sbooth@college.harvard.edu>
2015-09-29 17:03:52 -0400

    Took forever to figure out the shape of the data...

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,15 @@
 from P2 import *
+import findspark
+import os
+findspark.init('/home/serena/spark-1.5.0') # you need that before import pyspark.
+import pyspark 
 
 # Your code here
+sc = pyspark.SparkContext(appName="Spark1")
+#imRep = np.array([2000,2000])
+imData = sc.parallelize([[i,j],0] for i in range(2000) for j in range(2000))
+draw_image(imData)
+#while(True):
+#    draw_image(imData)
+#while(True):
+#    print "hi"
