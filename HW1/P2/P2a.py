16b43ef41fb4c5a210b201ad707640e5dd5292e8
Serena Booth <sbooth@college.harvard.edu>
2015-09-29 21:29:28 -0400

    Yay, printing set correctly

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,14 +1,20 @@
+from __future__ import print_function 
+
 from P2 import *
 import findspark
 import os
 findspark.init('/home/serena/spark-1.5.0') # you need that before import pyspark.
-import pyspark 
+import pyspark
 
 # Your code here
 sc = pyspark.SparkContext(appName="Spark1")
-#imRep = np.array([2000,2000])
-imData = sc.parallelize([[i,j],0] for i in range(2000) for j in range(2000))
+
+imData = sc.parallelize(([[i,j],mandelbrot(j/500.0 - 2, i/500.0 -2)] for i in range(2000) for j in range(2000)),100)
+#imData.foreach(lambda t : mandlebrot(t[0][1]/500.0 - 2, t[0][0]/500.0 - 2))
 draw_image(imData)
+#cite: http://stackoverflow.com/questions/25295277/view-rdd-contents-in-python-spark
+#sum_values_for_partitions(imData).foreach(print)
+
 #while(True):
 #    draw_image(imData)
 #while(True):
