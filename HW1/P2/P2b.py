ebe28be23ac01e56d615ac73f6fd4754d217e9dc
Serena Booth <sbooth@college.harvard.edu>
2015-09-30 13:30:19 -0400

    Error using mandelbrot(...) function, but otherwise partitioning OK for P2b

--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -1,3 +1,28 @@
 from P2 import *
+import findspark
+import os
+findspark.init('/home/serena/spark-1.5.0') # you need that before import pyspark.
+import pyspark
 
-# Your code here
+def mandel(x, y):
+    z = c = complex(x, y)
+    iteration = 0
+    max_iteration = 511  # arbitrary cutoff
+    while abs(z) < 2 and iteration < max_iteration:
+        z = z * z + c
+        iteration += 1
+    return iteration
+
+sc = pyspark.SparkContext(appName="Spark1")
+
+imData = sc.parallelize(([i,j] for i in range(2000) for j in range(2000)),100)
+imDataRepartitioned = imData.repartition(100)
+
+imDataWithMandlebrot = imDataRepartitioned.map(lambda row: [row, mandel(row[1]/500.0 - 2, row[0]/500.0 -2)])
+
+draw_image((imDataWithMandlebrot))
+
+plt.figure()
+plt.title("100 Random Partitions")
+plt.hist(sum_values_for_partitions((imDataWithMandlebrot)).collect())
+plt.show()
