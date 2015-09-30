6fddf1ea44534f5ec5cecf99cb3f230593234ba2
Serena Booth <sbooth@college.harvard.edu>
2015-09-30 17:07:40 -0400

    P2b now accessing mandelbrot(...) correctly

--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -4,21 +4,12 @@ import os
 findspark.init('/home/serena/spark-1.5.0') # you need that before import pyspark.
 import pyspark
 
-def mandel(x, y):
-    z = c = complex(x, y)
-    iteration = 0
-    max_iteration = 511  # arbitrary cutoff
-    while abs(z) < 2 and iteration < max_iteration:
-        z = z * z + c
-        iteration += 1
-    return iteration
-
 sc = pyspark.SparkContext(appName="Spark1")
 
 imData = sc.parallelize(([i,j] for i in range(2000) for j in range(2000)),100)
 imDataRepartitioned = imData.repartition(100)
 
-imDataWithMandlebrot = imDataRepartitioned.map(lambda row: [row, mandel(row[1]/500.0 - 2, row[0]/500.0 -2)])
+imDataWithMandlebrot = imDataRepartitioned.map(lambda row: [row, mandelbrot(row[1]/500.0 - 2, row[0]/500.0 -2)])
 
 draw_image((imDataWithMandlebrot))
 
