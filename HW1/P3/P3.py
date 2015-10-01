a78433b56f2fd8f6a5adce98286f3799933c13da
Serena Booth <sbooth@college.harvard.edu>
2015-09-30 20:22:02 -0400

    Anagram solver working

--- HW1/P3/P3.py
+++ HW1/P3/P3.py
@@ -9,16 +9,18 @@ import pyspark
 
 def sequence(word):
     letter_array = []
-
     for i in range(0,len(word)):
         letter_array += word[i:i+1]
 
-    return sorted(letter_array)
+    return str(sorted(letter_array))
 
 sc = pyspark.SparkContext(appName="Spark1")
 
 all_data = sc.textFile("./LF_delim/")
-all_data = all_data.repartition(100)
 
-all_data = all_data.map(lambda row: (sequence(row), row))
-print all_data.take(10)
+#words.map(lambda x: (x, 1)).reduceByKey(lambda x,y:x+y).map(lambda x:(x[1],x[0])).sortByKey(False)
+all_data = all_data.map(lambda row: (sequence(row), [row]),100)
+
+all_data = all_data.reduceByKey(lambda x,y: x + y)
+all_data = all_data.map(lambda row: (row[0], len(row[1]), row[1]))
+print all_data.takeOrdered(10, key = lambda row: -row[1])
